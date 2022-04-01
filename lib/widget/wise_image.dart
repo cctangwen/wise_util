a4a96e2b995c50861d879f6a705e62164858e64c
cctangwen <cctangwen@163.com>
2022-04-01 17:33:46 +0800

    Create wise_image.dart

---
+++ lib/widget/wise_image.dart
@@ -0,0 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+///网络图片组件
+class WiseImage extends StatelessWidget {
+  final double height;
+
+  final double width;
+
+  ///图片资源地址
+  final String src;
+
+  WiseImage({
+    required this.height,
+    required this.width,
+    required this.src,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.width,
+      height: this.height,
+      child: CachedNetworkImage(
+        fit: BoxFit.contain,
+        imageUrl: src,
+        errorWidget: (context, url, error) => _buildDefaultImage(),
+        placeholder: (context, url) => _buildDefaultImage(),
+      ),
+    );
+  }
+
+  _buildDefaultImage() => Container(
+        width: this.width,
+        height: this.height,
+        color: Colors.transparent,
+      );
+}
