7331aa697ab84ed71c6fb8b99a0a10dea3d8bc34
cctangwen <cctangwen@163.com>
2022-04-22 09:46:08 +0800

    Create wise_data_display.dart

---
+++ lib/widget/wise_data_display.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:wise_util/res/wise_style.dart';
+import 'package:wise_util/util/screen/screen_size_extension.dart';
+import 'package:wise_util/widget/wise_box.dart';
+import 'package:wise_util/widget/wise_divider.dart';
+
+class WiseDataDisplay {
+  static Widget buildVerticalData(String label, String? content,
+      {bool nullHide = true}) {
+    if (null == content && nullHide) return WiseBox().hBox0;
+    return Container(
+      padding: EdgeInsets.only(left: 16.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          WiseBox().hBox12,
+          Text(
+            label,
+            style: WiseStyle.textStyleSmallLabel(),
+          ),
+          // WiseBox().hBox4,
+          Text(
+            content ?? "-",
+            style: WiseStyle.textStyleLargeBody(),
+          ),
+          WiseBox().hBox12,
+          WiseDivider(),
+        ],
+      ),
+    );
+  }
+}
