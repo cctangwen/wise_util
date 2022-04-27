a579abd56f857359ca1e633dd49046d137366251
cctangwen <cctangwen@163.com>
2022-04-27 14:37:35 +0800

    Update language_util.dart

--- lib/util/language/language_util.dart
+++ lib/util/language/language_util.dart
@@ -1,29 +1,29 @@
 import 'package:wise_util/util/language/languages.dart';
 
 class LanguageUtil {
-  static String emptyString = "";
+  static String _emptyString = "";
 
   static String getLanguageLabel(String lang) {
     Map<String, dynamic>? country = languages[lang];
-    if (null == country) return emptyString;
+    if (null == country) return _emptyString;
     Map<String, dynamic>? localesLabel = country['localesLabel'];
     if (null == localesLabel) return country['label'];
     if (null != localesLabel[lang]) return localesLabel[lang];
     return country['label'];
   }
 
-  static String getRegionLabel(String lang) {
+  static String getRegionLabel(String lang, String locale) {
     Map<String, dynamic>? country = languages[lang];
-    if (null == country) return emptyString;
+    if (null == country) return _emptyString;
     Map<String, dynamic>? regionsLabel = country['regionsLabel'];
     if (null == regionsLabel) return country['label'];
-    if (null != regionsLabel[lang]) return regionsLabel[lang];
+    if (null != regionsLabel[locale]) return regionsLabel[locale];
     return country['label'];
   }
 
   static String getFlagIcon(String lang) {
     Map<String, dynamic>? country = languages[lang];
-    if (null == country) return emptyString;
+    if (null == country) return _emptyString;
     return country['icon'];
   }
 }
