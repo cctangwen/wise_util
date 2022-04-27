d649af18d837446a2fb61c171d7f50e9ea661af2
cctangwen <cctangwen@163.com>
2022-04-27 10:16:51 +0800

    Update wise_snack_bar.dart

--- lib/widget/wise_snack_bar.dart
+++ lib/widget/wise_snack_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wise_util/res/wise_localizations.dart';
 
 import '/util/screen/screen_size_extension.dart';
 
@@ -19,5 +20,20 @@ class WiseSnackBar {
       borderRadius: 10.r,
       backgroundColor: backgroundColor ?? Color(0xFF303030),
     ));
+  }
+
+  static showErrorSnackBar({
+    required String msg,
+    int duration = 3,
+    Color? backgroundColor,
+  }) {
+    WiseString strings =
+        WiseLocalizations.of(Get.context!)?.currentLocalization ??
+            EnWiseString();
+    showSnackBar(
+        title: strings.error,
+        msg: msg,
+        duration: duration,
+        backgroundColor: backgroundColor);
   }
 }
