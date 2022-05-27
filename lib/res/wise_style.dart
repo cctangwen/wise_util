a4680f0f17b25c0e3c61859a222ca676e3c334ff
cctangwen <cctangwen@163.com>
2022-05-27 14:45:25 +0800

    qq

--- lib/res/wise_style.dart
+++ lib/res/wise_style.dart
@@ -1,86 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import '/util/screen/screen_size_extension.dart';
 
 class WiseStyle {
   ///显示Display-适配深色模式
-  static TextStyle textStyleDisplay() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 36.0.sp,
-          fontWeight: FontWeight.w700));
+  static TextStyle textStyleDisplay() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 36.0.sp,
+      fontWeight: FontWeight.w700);
 
   ///大标题Headline-适配深色模式
-  static TextStyle textStyleHeadline() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 24.0.sp,
-          fontWeight: FontWeight.w600));
+  static TextStyle textStyleHeadline() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 24.0.sp,
+      fontWeight: FontWeight.w600);
 
   ///大号标题Title-适配深色模式
-  static TextStyle textStyleLargeTitle() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 20.0.sp,
-          fontWeight: FontWeight.w600));
+  static TextStyle textStyleLargeTitle() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 20.0.sp,
+      fontWeight: FontWeight.w600);
 
   ///中号标题Title-适配深色模式
-  static TextStyle textStyleMediumTitle() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 16.0.sp,
-          fontWeight: FontWeight.w600));
+  static TextStyle textStyleMediumTitle() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 16.0.sp,
+      fontWeight: FontWeight.w600);
 
   ///小号标题Title-适配深色模式
-  static TextStyle textStyleSmallTitle() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 14.0.sp,
-          fontWeight: FontWeight.w600));
+  static TextStyle textStyleSmallTitle() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 14.0.sp,
+      fontWeight: FontWeight.w600);
 
   ///大号正文Body-适配深色模式
-  static TextStyle textStyleLargeBody() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 16.0.sp,
-          fontWeight: FontWeight.w500));
+  static TextStyle textStyleLargeBody() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 16.0.sp,
+      fontWeight: FontWeight.w500);
 
   ///中号正文Body-适配深色模式
-  static TextStyle textStyleMediumBody() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 14.0.sp,
-          fontWeight: FontWeight.w500));
+  static TextStyle textStyleMediumBody() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 14.0.sp,
+      fontWeight: FontWeight.w500);
 
   ///小号正文Body-适配深色模式
-  static TextStyle textStyleSmallBody() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
-          fontSize: 12.0.sp,
-          fontWeight: FontWeight.w500));
+  static TextStyle textStyleSmallBody() => TextStyle(
+      fontFamily: 'Medium',
+      color: Get.isDarkMode ? Color(0xFFE2E2E5) : Color(0xFF1A1C1E),
+      fontSize: 12.0.sp,
+      fontWeight: FontWeight.w500);
 
   ///大号标签Label-适配深色模式
-  static TextStyle textStyleLargeLabel() => GoogleFonts.openSans(
-      textStyle: TextStyle(
-          color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
-          fontSize: 16.0.sp,
-          fontWeight: FontWeight.w400));
+  static TextStyle textStyleLargeLabel() => TextStyle(
+      fontFamily: 'Regular',
+      color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
+      fontSize: 16.0.sp,
+      fontWeight: FontWeight.w400);
 
   ///中号标签Label-适配深色模式
-  static TextStyle textStyleMediumLabel() => GoogleFonts.openSans(
-        textStyle: TextStyle(
-            color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
-            fontSize: 14.0.sp,
-            fontWeight: FontWeight.w400),
-      );
+  static TextStyle textStyleMediumLabel() => TextStyle(
+      fontFamily: 'Regular',
+      color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
+      fontSize: 14.0.sp,
+      fontWeight: FontWeight.w400);
 
   ///小号标签Label-适配深色模式
-  static TextStyle textStyleSmallLabel() => GoogleFonts.openSans(
-        textStyle: TextStyle(
-            color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
-            fontSize: 12.0.sp,
-            fontWeight: FontWeight.w400),
-      );
+  static TextStyle textStyleSmallLabel() => TextStyle(
+      fontFamily: 'Regular',
+      color: Get.isDarkMode ? Color(0xFF8B9198) : Color(0xFF41474D),
+      fontSize: 12.0.sp,
+      fontWeight: FontWeight.w400);
 }
